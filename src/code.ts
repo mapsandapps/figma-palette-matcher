ff61b8b0580575f936ac39219caeca7e47f67a8c
Shawn Taylor <shawn@mapsandapps.net>
2021-10-09 22:40:55 -0400

    Use chroma-js

--- src/code.ts
+++ src/code.ts
@@ -1,8 +1,18 @@
+import chroma from 'chroma-js'
+
 figma.showUI(__html__)
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === "match-color") {
-    console.log(msg.color)
+    let color = chroma.hex('#000000').css()
+    try {
+      color = chroma.hex(msg.color).css()
+      console.log(color)
+    } catch (e) {
+      // TODO: send warning to user
+      console.warn('Could not parse color', msg.color)
+    }
+
     // const nodes = [];
 
     // for (let i = 0; i < msg.count; i++) {
--- src/ui.ts
+++ src/ui.ts
@@ -3,8 +3,8 @@ import "./ui.css"
 document.getElementById("match").onclick = () => {
   const textbox = document.getElementById("color") as HTMLInputElement
   parent.postMessage({ pluginMessage: { type: "match-color", color: textbox.value } }, "*")
-};
+}
 
 document.getElementById("cancel").onclick = () => {
   parent.postMessage({ pluginMessage: { type: "cancel" } }, "*")
-};
+}
